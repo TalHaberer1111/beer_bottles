c3ac1f33dfc6ef4c2e1a4a5c133f77e47056bc37
emmanuel cutler and tal haberer <student@epicodus.com>
2016-01-13 14:20:15 -0800

    Initial commit

---
+++ js/ice.js
@@ -0,0 +1,27 @@
+$(document).ready(function() {
+  var bottles = 99,
+      bottle = "bottles",
+      text = "",
+      output = document.getElementById('output');
+  while (bottles > 0) {
+      if (bottles == 1) {
+          bottle = "bottle";
+      }
+
+      text += bottles + " ";
+      text += bottle + " of beer on the wall, ";
+      text += bottles + " " + bottle + " of beer.<br>";
+
+      bottles--;
+      text += "Take one down and pass it around, ";
+      text += + bottles + " bottles of beer on the wall.<hr>"
+
+      if (bottles == 0) {
+          bottles = "no more";
+      }
+      output.innerHTML += text;
+    text = '';
+  }
+
+  output.innerHTML +=  " No more bottles of beer on the wall, no more bottles of beer. ";
+});
